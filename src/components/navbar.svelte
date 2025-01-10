4b6b2a645e35851b578b178f31f499beb6057fb5
Anav Chand <anavkki@gmail.com>
2025-01-11 11:46:16 +1200

    feat: add navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { getStores } from "$app/stores"
+	const { page } = getStores()
+
+	const links = [
+		{
+			label: "Home",
+			href: "/"
+		},
+		{
+			label: "Counter",
+			href: "/counter"
+		},
+		{
+			label: "Temperature Converter",
+			href: "/temperature-converter"
+		},
+		{
+			label: "Flight Booking",
+			href: "/flight-booking"
+		}
+	]
+</script>
+
+<nav>
+	{#each links as { label, href }}
+		<a {href} class={`${$page.url.pathname == href && "text-orange-600"}`}>{label}</a>
+	{/each}
+</nav>
+
+<style>
+	nav {
+		@apply flex flex-col w-80 border-r border-zinc-200;
+
+		a {
+			@apply p-5 border-b border-zinc-200;
+		}
+	}
+</style>

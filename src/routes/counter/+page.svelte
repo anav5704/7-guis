ee6ad750c64815286c480f3265f627ee8b2ae1a0
Anav Chand <anavkki@gmail.com>
2025-01-11 11:46:43 +1200

    feat: add counter

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let count = $state(0);
+</script>
+
+<div class="space-x-5">
+	<button onclick={() => count--}>-</button>
+	<input type="number" disabled bind:value={count} />
+	<button onclick={() => count++}>+</button>
+</div>
+
+<style>
+	input,
+	button {
+		@apply rounded-lg border border-zinc-200 py-3 px-6;
+	}
+</style>

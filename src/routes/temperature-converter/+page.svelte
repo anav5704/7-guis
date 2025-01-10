3dfa98d544fb75c4d07ed6ea678b6248c07b2020
Anav Chand <anavkki@gmail.com>
2025-01-11 11:46:58 +1200

    feat: add temperature converter

---
+++ src/routes/temperature-converter/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	class Temperature {
+		#c = $state(0);
+		#f = $state(0);
+
+		get c() {
+			return this.#c;
+		}
+
+		set c(temperature) {
+			this.#c = temperature;
+			this.#f = temperature * (9 / 5) + 32;
+		}
+
+		get f() {
+			return this.#f;
+		}
+
+		set f(temperature) {
+			this.#f = temperature;
+			this.c = (temperature - 32) * (5 / 9);
+		}
+	}
+
+	const temperature = new Temperature();
+</script>
+
+<div class="space-x-5">
+	<input type="number" bind:value={temperature.c} />
+	<input type="number" bind:value={temperature.f} />
+</div>
+
+<style>
+	input {
+		@apply rounded-lg border border-zinc-200 py-3 px-6;
+	}
+</style>

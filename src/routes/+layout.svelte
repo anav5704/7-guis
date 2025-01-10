4438d747014a530880f65253c655686850c51b6a
Anav Chand <anavkki@gmail.com>
2025-01-11 11:46:39 +1200

    feat: add main layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Navbar from "../components/navbar.svelte"
+	import "../app.css"
+
+	let { children } = $props()
+</script>
+
+<main class="flex">
+	<Navbar />
+	<div class="grid place-content-center h-screen w-screen">
+		{@render children()}
+	</div>
+</main>
